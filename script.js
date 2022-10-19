4a90172b66d0b0b29b8ddd96d0354d08c9df543f
Ali Yasin Emir <89742020+ali-yasin-emir@users.noreply.github.com>
2022-10-19 13:22:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let country = "Türkiye";
+console.log(country);
+
+let continent = "Asia and Europe";
+console.log(continent);
+
+let population = "84 Million";
+console.log(typeof population);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+const mother = "Hülya";
+// anne ismi her zaman aynı kalır(biyolojik anne).
+
+const yasin_birthDate = 2000;
+// doğum tarihi her kişi için sabittir ve tekrar değiştirilemez.
